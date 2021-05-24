4b9a2a5fb4422a49bb157823dda9332678fc2d8c
201-02\user <kaka4481@gamil.com>
2021-05-24 15:10:35 +0900

    게시판 디자인 수정

--- justdoeat/src/main/java/com/justdoeat/web/MemberController.java
+++ justdoeat/src/main/java/com/justdoeat/web/MemberController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import common.CommonService;
-import member.MemberDTO;
+
 import member.MemberServiceImpl;
 import member.MemberVO;
 
